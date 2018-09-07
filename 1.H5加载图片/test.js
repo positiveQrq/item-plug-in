64b5f537b6d7f71f1b8aedda25c8cb50ff31bcd8
posityle <runqiang_qiao@sina.com>
2018-09-07 11:04:44 +0800

    2

--- 1.H5加载图片/test.js
+++ 1.H5加载图片/test.js
@@ -1,6 +1,6 @@
 page.config({
-    url:'http://wx.iddi-inc.com/css/wx/app/180801/images/',
-    staticImg: [
+    url:'http://wx.iddi-inc.com/css/wx/app/180801/images/',//图片路径
+    staticImg: [ //静态文件名
         'loading-all.jpg', 
         'lun_03.png',
         'loading-sign.png',
@@ -14,7 +14,7 @@ page.config({
         'movie2.jpg',      
         'movie_3.jpg',               
     ],
-    loadingfn:function(loadingNumber) { //加载中执行函数, 记载完成执行函数
+    loadingfn:function(loadingNumber) { //加载处理函数
         console.log(loadingNumber)
         $('.js-loading-number').text(loadingNumber + '%');
         if(loadingNumber == 100) {
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # item-plug-in
 项目中使用小插件
+
+# h5加载图片
+
+	调用方式：
+		page.config({
+			url: '',  //图片绝对路径 
+			staticImg: [],  //图片名称
+			loadingfn:function(loadingNumber) { //加载处理函数
+		        console.log(loadingNumber)
+		    }
+		})
